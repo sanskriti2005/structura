0152c05e8f35f99d894e3fb1a56fb652abdd0247
Sanskriti <sanskritisri27@gmail.com>
2025-04-05 21:25:39 +0530

    built the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <h1>Structura</h1>
       <Button>Shadcn btn</Button>
     </div>
   );
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import { Button } from '@/components/ui/button';
+
 export default function Header() {
     return (
-        <header>
-            Header
-        </header>
+        <nav className="container flex items-center justify-between py-4 px-4 lg:px-8  mx-auto">
+            <div><Link href='/'>Structura</Link></div>
+            <div><Link href="/#pricing">Pricing</Link></div>
+            <div>
+                <Button>Sign-In</Button>
+            </div>
+        </nav>
     );
 }
